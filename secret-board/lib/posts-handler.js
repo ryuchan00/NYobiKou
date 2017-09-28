fb10289ffd0811ad11f7fac578c4757da6273d8d
yamakawa00 <leokun0210@gmail.com>
2017-09-28 03:39:02 +0000

    update

---
+++ secret-board/lib/posts-handler.js
@@ -0,0 +1,39 @@
+'use strict';
+const jade = require('jade');
+const contents = [];
+
+function handle(req ,res) {
+    switch  (req.method) {
+        case 'GET':
+            // res.end('hi');
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            res.end(jade.renderFile('./views/posts.jade', {}));
+            break;
+        case 'POST':
+            req.on('data', (data) => {
+                const decoded = decodeURIComponent(data);
+            const content = decoded.split('content=')[1];
+            console.info('投稿されました: ' + content);
+            contents.push(content);
+            console.info('保存されました: ' + content);
+            console.info(contents);
+            handleRedirectPosts(req, res);
+            });
+            break;
+        default:
+            break;
+    }
+}
+
+function handleRedirectPosts(req, res) {
+    res.writeHead(303, {
+       'Location': '/posts'
+    });
+    res.end();
+}
+
+module.exports = {
+    handle: handle
+};
