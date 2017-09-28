5c637e587816cbcddcaca7f40a697e355530be2f
yamakawa00 <leokun0210@gmail.com>
2017-09-28 06:05:08 +0000

    update

---
+++ secret-board-3021/lib/posts-handler.js
@@ -0,0 +1,40 @@
+'use strict';
+const jade = require('jade');
+const util = require('./handler-util')
+const contents = [];
+
+function handle(req, res) {
+  switch (req.method) {
+    case 'GET':
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+      });
+      // res.end(jade.renderFile('./views/posts.jade', {}));
+      res.end(jade.renderFile('./views/posts.jade', { contents: contents}));
+      break;
+    case 'POST':
+      req.on('data', (data) => {
+        const decoded = decodeURIComponent(data);
+        const content = decoded.split('content=')[1];
+        console.info('投稿されました: ' + content);
+        contents.push(content);
+        console.info('投稿された全内容: ' + contents);
+        handleRedirectPosts(req, res);
+      });
+      break;
+    default:
+      util.handleBadRequest(req, res);
+      break;
+  }
+}
+
+function handleRedirectPosts(req, res) {
+  res.writeHead(303, {
+    'Location': '/posts'
+  });
+  res.end();
+}
+
+module.exports = {
+  handle: handle
+};
---
+++ secret-board-3021/lib/router.js
@@ -0,0 +1,29 @@
+'use strict';
+const postsHandler = require('./posts-handler');
+const util = require('./handler-util');
+
+function route(req, res) {
+    // switch (req.method) {
+    //     case 'GET':
+    //     case 'POST':
+    switch (req.url) {
+        case '/posts':
+            postsHandler.handle(req, res);
+            break;
+        case '/logout':
+            util.handleLogout(req, res);
+            break;
+        default:
+            util.handleNotFound(req, res);
+            break;
+    }
+    // break;
+    // default:
+    //     util.handleBadRequest(req, res);
+    //     break;
+    // }
+}
+
+module.exports = {
+    route: route
+};
