cb2f499d5b96a73d855635ed72c4298cf607a8e8
yamakawa00 <leokun0210@gmail.com>
2017-10-01 02:03:28 +0000

    update

---
+++ secret-board-3030/lib/posts-handler.js
@@ -0,0 +1,149 @@
+'use strict';
+const crypto = require('crypto');
+const jade = require('jade');
+const Cookies = require('cookies');
+const moment = require('moment-timezone');
+const util = require('./handler-util');
+const Post = require('./post');
+
+const trackingIdKey = 'tracking_id';
+
+function handle(req, res) {
+    const cookies = new Cookies(req, res);
+    // addTrackingCookie(cookies);
+
+    const trackingId = addTrackingCookie(cookies, req.user);
+
+    switch (req.method) {
+        case 'GET':
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            Post.findAll({order: 'id DESC'}).then((posts) => {
+                posts.forEach((post) => {
+                    post.content = post.content.replace(/\+/g, ' ');
+                    post.formattedCreatedAt = moment(post.createdAt).tz('Asia/Tokyo').format('YYYY年MM月DD日 HH時mm分ss秒');
+                });
+                res.end(jade.renderFile('./views/posts.jade', {
+                    posts: posts,
+                    user: req.user
+                }));
+                console.info(
+                    `閲覧されました: user: ${req.user}, ` +
+                    // `trackinId: ${cookies.get(trackingIdKey) },` +
+                    `trackingId: ${trackingId},` +
+                    `remoteAddress: ${req.connection.remoteAddress}, ` +
+                    `userAgent: ${req.headers['user-agent']} `
+                );
+            });
+            break;
+        case 'POST':
+            let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                const decoded = decodeURIComponent(body);
+                const content = decoded.split('content=')[1];
+                console.info('投稿されました: ' + content);
+                Post.create({
+                    content: content,
+                    // trackingCookie: cookies.get(trackingIdKey),
+                    trackingCookie: trackingId,
+                    postedBy: req.user
+                }).then(() => {
+                    handleRedirectPosts(req, res);
+                });
+            });
+            break;
+        default:
+            util.handleBadRequest(req, res);
+            break;
+    }
+}
+
+function handleDelete(req, res) {
+    switch (req.method) {
+        case 'POST':
+            let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                const decoded = decodeURIComponent(body);
+                const id = decoded.split('id=')[1];
+                Post.findById(id).then((post) => {
+                    if (req.user === post.postedBy || req.user === 'admin') {
+                        post.destroy();
+                        console.info(
+                            `削除されました: user: ${req.user}, ` +
+                            `remoteAddress: ${req.connection.remoteAddress}, ` +
+                            `userAgent: ${req.headers['user-agent']} `
+                        );
+                    }
+                    handleRedirectPosts(req, res);
+                });
+            });
+            break;
+        default:
+            util.handleBadRequest(req, res);
+            break;
+    }
+}
+
+/**
+ * Cookieに含まれているトラッキングIDに異常がなければその値を返し、
+ * 存在しない場合や異常なものである場合には、再度作成しCookieに付与してその値を返す
+ * @param {Cookies} cookies
+ * @param {String} userName
+ * @return {String} トラッキングID
+ */
+function addTrackingCookie(cookies, userName) {
+    const requestedTrakignId = cookies.get(trackingIdKey);
+    if (isValidTrackingId(requestedTrakignId, userName)) {
+        return requestedTrakignId;
+    } else {
+        const originalId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+        const tomorrow = new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
+        const trackingId = originalId + '_' + createValidHash(originalId, userName);
+        cookies.set(trackingIdKey, trackingId, {expires: tomorrow});
+        return trackingId;
+    }
+}
+
+function isValidTrackingId(trackingId, userName) {
+    if (!trackingId) {
+        return false;
+    }
+    const splitted = trackingId.split('_');
+    const originalId = splitted[0];
+    const requestedHash = splitted[1];
+    return createValidHash(originalId, userName) === requestedHash;
+}
+
+function createValidHash(originalId, userName) {
+    const sha1sum = crypto.createHash('sha1');
+    sha1sum.update(originalId + userName);
+    //最終的にそのメッセージダイジェストを、 16 進数の文字列として取得しています。
+    return sha1sum.digest('hex');
+}
+
+// function addTrackingCookie(cookies) {
+//     if (!cookies.get(trackingIdKey)) {
+//         const trackingId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+//         const tomorrow = new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
+//         cookies.set(trackingIdKey, trackingId, {expires: tomorrow});
+//     }
+// }
+
+function handleRedirectPosts(req, res) {
+    res.writeHead(303, {
+        'Location': '/posts'
+    });
+    res.end();
+}
+
+module.exports = {
+    handle: handle,
+    handleDelete: handleDelete
+};
