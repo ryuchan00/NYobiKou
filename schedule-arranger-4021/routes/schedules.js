8c2252d86777a6a077bb1f994deacd8ee0053647
yamakawa00 <leokun0210@gmail.com>
2017-11-01 23:53:13 +0000

    Add schedule comment edit in production

--- schedule-arranger-4021/routes/schedules.js
+++ schedule-arranger-4021/routes/schedules.js
@@ -159,16 +159,62 @@ function isMine(req, schedule) {
     return schedule && parseInt(schedule.createdBy) === parseInt(req.user.id);
 }
 
-router.post('/:scheduleId', authenticationEnsurer, (req, res, next) => {
++router.post('/:scheduleId', authenticationEnsurer, (req, res, next) => {
     if (parseInt(req.query.edit) === 1) {
-        where: {
-            scheduleId: req.params.scheduleId
-        }
-    }).
-    then((schedule) => {
-
-    });
-
+        Schedule.findOne({
+            where: {
+                scheduleId: req.params.scheduleId
+            }
+        }).then((schedule) => {
+            if (isMine(req, schedule)) { // 作成者のみ
+                const updatedAt = new Date();
+                schedule.update({
+                    scheduleId: schedule.scheduleId,
+                    scheduleName: req.body.scheduleName.slice(0, 255),
+                    memo: req.body.memo,
+                    createdBy: req.user.id,
+                    updatedAt: updatedAt
+                }).then((schedule) => {
+                    Candidate.findAll({
+                        where: {scheduleId: schedule.scheduleId},
+                        order: '"candidateId" ASC'
+                    }).then((candidates) => {
+                        // 追加されているかチェック
+                        const candidateNames = parseCandidateNames(req);
+                        if (candidateNames) {
+                            createCandidatesAndRedirect(candidateNames, schedule.scheduleId, res);
+                        } else {
+                            res.redirect('/schedules/' + schedule.scheduleId);
+                        }
+                    });
+                });
+            } else {
+                const err = new Error('指定された予定がない、または、編集する権限がありません');
+                err.status = 404;
+                next(err);
+            }
+        });
+    } else {
+        const err = new Error('不正なリクエストです');
+        err.status = 400;
+        next(err);
+    }
 });
+
+function createCandidatesAndRedirect(candidateNames, scheduleId, res) {
+    const candidates = candidateNames.map((c) => {
+        return {
+            candidateName: c,
+            scheduleId: scheduleId
+        };
+    });
+    Candidate.bulkCreate(candidates).then(() => {
+        res.redirect('/schedules/' + scheduleId);
+    });
+}
+
+function parseCandidateNames(req) {
+    return req.body.candidates.trim().split('\n').map((s) => s.trim());
+}
 
 module.exports = router;
